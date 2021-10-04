d7253cd7b604a187f96ecf75357874e4f3019683
Priyansh Choudhary <im1706@gmail.com>
2021-10-04 13:56:15 +0530

    Update Portfolio Operations to Maximize the Profit.cpp

--- Easy/Portfolio Operations to Maximize the Profit.cpp
+++ Easy/Portfolio Operations to Maximize the Profit.cpp
@@ -2,28 +2,26 @@
 using namespace std;
 
 int portfolio_profit_maximisation(int maxSum, vector<int> a, vector<int> b) {
-    int index1=0,index2=0;
-    int cost=0,count=0;
-    int n1=a.size(),n2=b.size();
-    while(index1<n1 && index2<n2 && cost<=maxSum){
-        if(a[index1]<=b[index2])
-            { if(cost+a[index1]>maxSum) return count; 
-            cost+=a[index1]; index1++; count++;}
-        else
-            { if(cost+b[index2]>maxSum) return count;
-            cost+=b[index2]; index2++; count++;}
+    int index=-1,sum=0,index2=-1;
+    while(index+1<a.size() && sum+a[index+1]<=maxSum){
+        index++;
+        sum+=a[index];
     }
-    
-    while(index1<n1 && cost<=maxSum)
-        { if(cost+a[index1]>maxSum) return count;
-        cost+=a[index1]; index1++; count++;}
-    
-    while(index2<n2 && cost<=maxSum)
-        { if(cost+b[index2]>maxSum) return count;
-        cost+=b[index2]; index2++; count++;}
-    
-    return count;
-    
+    int answer=index+1;
+    while(index2+1<b.size()){
+        if(sum+b[index2+1]<=maxSum){
+            index2++;
+            sum+=b[index2];
+            answer=max(answer,index+1+index2+1);
+        }else{
+            if(index==-1) break;
+            else {
+                sum-=a[index];
+                index--;
+            }
+        }
+    }
+    return answer;
 }
 
 int main()
