0f22dc53103c83b5e5e9bf03ba555524a7a330bd
Priyansh Choudhary <im1706@gmail.com>
2021-10-08 10:21:26 +0530

    Update Stable Partition.cpp

--- Hard/Stable Partition.cpp
+++ Hard/Stable Partition.cpp
@@ -22,7 +22,42 @@
 #include <unordered_map>
 
 using namespace std;
+
+void printSets(set<int> s1,set<int> s2){
+    for(auto i:s1)
+        cout<<i<<" ";
+        cout<<endl;
+    for(auto i:s2)
+        cout<<i<<" ";
+        cout<<endl;
+}
+
+
+
 void countStablePartitions(int n, vector<int>& values) {
+    set<int> s1,s2;
+    for(auto val:values)
+        if(val&1) s2.insert(val);
+        else s1.insert(val);
+    printSets(s1,s2);
+    for(auto i=s2.begin();i!=s2.end();i++){
+        bool found=false;
+        for(auto j=s1.begin();j!=s1.end();j++)
+            {
+                if(*i==1)  break;
+                int val=*i>*j?*i%*j:*j%*i;
+                if(!val){
+                    s1.insert(*i);
+                    s2.erase(i);
+                    found=true;
+                    break;
+                }
+            }
+    }
+    printSets(s1,s2);
+    int result= 2*((1<<s2.size())-1);
+    if(result==0) cout<<"NO\n0\n";
+    else cout<<"YES\n"<<result<<endl;
 }
 
 int main(){
